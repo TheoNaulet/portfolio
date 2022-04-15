8c416d3ca7b314ebc4a2c482fc3fc1968323fd0c
Théo Naulet <theo.naulet@gmail.com>
2022-04-15 19:16:47 +0200

    Lignes autour des titres, icones Linkedin/GitHub

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import github from './logo-github.png';
+import React from 'react';
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Alex+Brush&display=swap');
@@ -10,173 +12,189 @@ import 'react-circular-progressbar/dist/styles.css';
 </style>;
 
 
+
 function App() {
   return (
     <div className="App">
-      <div className="me">
-        <div className='left-me'>
-          <div className="name">
-            Théo Naulet
-          </div>
-          <div className='presentation'>
-            <p className='welcome'>WELCOME</p>
-            <h1 className='theo'>I'm Theo Naulet</h1>
-            <p className='profession'>Étudiant en web-developpement</p>
-            <div className='dl-cv'>
-              <p className='dl'>Download CV</p>
+        <div className="me">
+          <div className='left-me'>
+            <div className="name">
+              Théo Naulet
+            </div>
+            <div className='presentation'>
+              <p className='welcome'>WELCOME</p>
+              <h1 className='theo'>I'm Théo Naulet</h1>
+              <p className='profession'>Étudiant en web-developpement</p>
+              <div className='dl-cv'>
+                <p className='dl'>Download CV</p>
+              </div>
             </div>
           </div>
-        </div>
-          <div className='pic-me'><img src={require("./me.png")}></img></div>
-          <div className='menu'>
-              <ul>
-                <li>Home</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
-              </ul>
+            <div className='pic-me'><img src={require("./me.png")}></img></div>
+            <div className='menu'>
+                <ul>
+                  <li>Home</li>
+                  <li>Portfolio</li>
+                  <li>Contact</li>
+                </ul>
+            </div>
           </div>
-        </div>
-        <div className='about'>
-          <div className='bloc-image-about'>
-            <img class='image-about' src={require("./me.png")}></img>
+          <div className='about'>
+            <div className='bloc-image-about'>
+              <img class='image-about' src={require("./me.png")}></img>
+            </div>
+            <div className='text-about'>
+              <h1>About</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sollicitudin ut augue netus fusce fringilla. Aliquam blandit enim odio cursus id. Pulvinar commodo pellentesque non massa, nunc massa nisi est id. Diam est molestie massa commodo pulvinar aliquam arcu gravida. Dictum pretium senectus dolor vulputate tempor ac vel. Placerat proin sed risus at fusce aliquam. Convallis varius auctor quam fusce diam. Vulputate enim est nibh diam pellentesque. Adipiscing dolor turpis egestas velit sit feugiat ultricies.
+                  Id ac placerat dolor sem neque elit. Ut nisl turpis nisi etiam ac nibh.</p>
+            </div>
           </div>
-          <div className='text-about'>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sollicitudin ut augue netus fusce fringilla. Aliquam blandit enim odio cursus id. Pulvinar commodo pellentesque non massa, nunc massa nisi est id. Diam est molestie massa commodo pulvinar aliquam arcu gravida. Dictum pretium senectus dolor vulputate tempor ac vel. Placerat proin sed risus at fusce aliquam. Convallis varius auctor quam fusce diam. Vulputate enim est nibh diam pellentesque. Adipiscing dolor turpis egestas velit sit feugiat ultricies.
-                Id ac placerat dolor sem neque elit. Ut nisl turpis nisi etiam ac nibh.</p>
-          </div>
-        </div>
-        <div className='skills'>
-          <div className='titre1'>
-            <h1 className='title'>My Skills</h1>
-          </div>
-          <div className='my-skills'>
-            <div className='bar' id='skills1'>
-              <CircularProgressbarWithChildren
-                value={50} 
-                styles={{ 
-                root: {}, 
-                path: {
-                  stroke: `rgb(61, 34, 254)`,
-                },
-              }}
-                >
+          <div className='skills'>
+            <div className='titre1'>
+            <h4><span>My skills</span></h4>
+            </div>
+            <div className='my-skills'>
+              <div className='bar' id='skills1'>
+                <CircularProgressbarWithChildren
+                  value={50} 
+                  styles={{ 
+                  root: {}, 
+                  path: {
+                    stroke: `rgb(21, 198, 203)`,
+                  },
+                }}
+                  >
 
-                {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
-                <img style={{ width: 60, marginTop: -5, marginBottom: 10 }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png" alt="doge" />
-                <div style={{ fontSize: 12, marginTop: -5 }}>
-                  <strong>50%</strong>
-                </div>
-              </CircularProgressbarWithChildren>
-            </div>
-            <div className='bar' id='skills2'>
-            <CircularProgressbarWithChildren 
-                value={80}
-                styles={{ 
-                  root: {}, 
-                  path: {
-                    stroke: `rgb(61, 34, 254)`,
-                  },
-                }} >
-                {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
-                <img style={{ width: 60, marginTop: -5, marginBottom: 10 }} src="https://cdn-icons-png.flaticon.com/512/1216/1216733.png" alt="doge" />
-                <div style={{ fontSize: 12, marginTop: -5 }}>
-                  <strong>80%</strong>
-                </div>
-              </CircularProgressbarWithChildren>
-            </div>
-            <div className='bar' id='skills3'>
-            <CircularProgressbarWithChildren 
-                value={70}
-                styles={{ 
-                  root: {}, 
-                  path: {
-                    stroke: `rgb(61, 34, 254)`,
-                  },
-                }}>
-                {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
-                <img style={{ width: 60, marginTop: -5, marginBottom: 10 }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Devicon-css3-plain.svg/2048px-Devicon-css3-plain.svg.png" alt="doge" />
-                <div style={{ fontSize: 12, marginTop: -5 }}>
-                  <strong>70%</strong>
-                </div>
-              </CircularProgressbarWithChildren>
-            </div>
-            <div className='bar' id='skills4'>
-            <CircularProgressbarWithChildren 
-                value={20}
-                styles={{ 
-                  root: {}, 
-                  path: {
-                    stroke: `rgb(61, 34, 254)`,
-                  },
-                }}>
-                {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
-                <img style={{ width: 60, marginTop: -5, marginBottom: 10 }} src="https://grafikart.fr/uploads/icons/react.svg" alt="doge" />
-                <div style={{ fontSize: 12, marginTop: -5 }}>
-                  <strong>20%</strong>
-                </div>
-              </CircularProgressbarWithChildren>
-            </div>
-            <div className='bar' id='skills5'>
-            <CircularProgressbarWithChildren 
-                  value={90}
+                  {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
+                  <img style={{ width: 60, marginTop: -5, marginBottom: 10 }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png" alt="doge" />
+                  <div style={{ fontSize: 12, marginTop: -5 }}>
+                    <strong>50%</strong>
+                  </div>
+                </CircularProgressbarWithChildren>
+              </div>
+              <div className='bar' id='skills2'>
+              <CircularProgressbarWithChildren 
+                  value={80}
                   styles={{ 
                     root: {}, 
                     path: {
-                      stroke: `rgb(61, 34, 254)`,
+                      stroke: `rgb(21, 198, 203)`,
+                    },
+                  }} >
+                  {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
+                  <img style={{ width: 60, marginTop: -5, marginBottom: 10 }} src="https://cdn-icons-png.flaticon.com/512/1216/1216733.png" alt="doge" />
+                  <div style={{ fontSize: 12, marginTop: -5 }}>
+                    <strong>80%</strong>
+                  </div>
+                </CircularProgressbarWithChildren>
+              </div>
+              <div className='bar' id='skills3'>
+              <CircularProgressbarWithChildren 
+                  value={70}
+                  styles={{ 
+                    root: {}, 
+                    path: {
+                      stroke: `rgb(21, 198, 203)`,
                     },
                   }}>
-                {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
-                <img style={{ width: 60, marginTop: -5, marginBottom: 10 }} src="https://hdf-formation.com/wp-content/uploads/2021/12/adobe-photoshop-logo-freelogovectors.net_.png" alt="doge"/>
-                <div style={{ fontSize: 12, marginTop: -5 }}>
-                  <strong>90%</strong>
+                  {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
+                  <img style={{ width: 60, marginTop: -5, marginBottom: 10 }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Devicon-css3-plain.svg/2048px-Devicon-css3-plain.svg.png" alt="doge" />
+                  <div style={{ fontSize: 12, marginTop: -5 }}>
+                    <strong>70%</strong>
+                  </div>
+                </CircularProgressbarWithChildren>
+              </div>
+              <div className='bar' id='skills4'>
+              <CircularProgressbarWithChildren 
+                  value={20}
+                  styles={{ 
+                    root: {}, 
+                    path: {
+                      stroke: `rgb(21, 198, 203)`,
+                    },
+                  }}>
+                  {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
+                  <img style={{ width: 60, marginTop: -5, marginBottom: 10 }} src="https://grafikart.fr/uploads/icons/react.svg" alt="doge" />
+                  <div style={{ fontSize: 12, marginTop: -5 }}>
+                    <strong>20%</strong>
+                  </div>
+                </CircularProgressbarWithChildren>
+              </div>
+              <div className='bar' id='skills5'>
+              <CircularProgressbarWithChildren 
+                    value={90}
+                    styles={{ 
+                      root: {}, 
+                      path: {
+                        stroke: `rgb(21, 198, 203)`,
+                      },
+                    }}>
+                  {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
+                  <img style={{ width: 60, marginTop: -5, marginBottom: 10 }} src="https://hdf-formation.com/wp-content/uploads/2021/12/adobe-photoshop-logo-freelogovectors.net_.png" alt="doge"/>
+                  <div style={{ fontSize: 12, marginTop: -5 }}>
+                    <strong>90%</strong>
+                  </div>
+                </CircularProgressbarWithChildren>
+              </div>
+            </div>
+            <div className='bottom-skills'>
+              <div className='language'>
+                <p className='bigtitle'>Language</p>
+                  <p>English</p>
+                  <div className='jaugeEN'></div>
+                  <p>French</p>
+                  <div className='jaugeFR'></div>
+              </div>
+              <div className='studies'>
+                <p className='bigtitle'>Studies</p>
+                <div className='study'>
+                    <div className='ball'></div>
+                    <div className='text-stud'>
+                      <p className='place'>Bachelor Sport Event</p>
+                      <p className='year'>2019 - 2021</p>
+                    </div>
                 </div>
-              </CircularProgressbarWithChildren>
-            </div>
-          </div>
-          <div className='bottom-skills'>
-            <div className='language'>
-              <p className='bigtitle'>Language</p>
-                <p>English</p>
-                <div className='jaugeEN'></div>
-                <p>French</p>
-                <div className='jaugeFR'></div>
-            </div>
-            <div className='studies'>
-              <p className='bigtitle'>Studies</p>
-              <div className='study'>
-                  <div className='ball'></div>
-                  <div className='text-stud'>
-                    <p className='place'>Bachelor Sport Event</p>
-                    <p className='year'>2019 - 2021</p>
-                  </div>
-              </div>
-              <div className='study'>
-                  <div className='ball'></div>
-                  <div className='text-stud'>
-                    <p className='place'>Baccalauréat STI2D</p>
-                    <p className='year'>2017</p>
-                  </div>
+                <div className='study'>
+                    <div className='ball'></div>
+                    <div className='text-stud'>
+                      <p className='place'>Baccalauréat STI2D</p>
+                      <p className='year'>2017</p>
+                    </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className='xp'>
-          <div className='bandeauxp'>
-          <p className='bigtitle' id='workxp'>My work experience</p>
-          </div>
-          <div className='wrapper'>
-            <div className='work' id="work1">
-              <p>Conseiller de vente multimédia</p>
+          <div className='xp'>
+            <div className='bandeauxp'>
+              <h2><span>My work experience</span></h2>
             </div>
-            <div className='work' id="work2"></div>
-            <div className='work' id="work3"></div>
-            <div className='work' id="work4" ></div>
+            <div className='wrapper'>
+              <div className='work' id="work1">
+                <div className='description'>
+                  <p className='date'>Nov. 2021 - Déc. 2021</p>
+                  <p className='poste'>Conseiller de vente multimédia</p>
+                </div>
+              </div>
+              <div className='work' id="work2"></div>
+              <div className='work' id="work3"></div>
+              <div className='work' id="work4" ></div>
+            </div>
           </div>
-        </div>
-        <div className='contact'>
-
-        </div>
+          <div className='bottom'>
+            <div className='contact'>
+              <h3>Based in Montpellier</h3>
+              <a class="email" href='mailto:theo.naulet@gmail.com'>theo.naulet@gmail.com</a>
+              <p className='number'>+33 7 81 36 41 76</p>     
+            </div>
+            <div className='linkedin'>
+              <a className='linkedinlogo' target="_blank" href='https://www.linkedin.com/in/theo-naulet/'>
+                <img src='https://pnggrid.com/wp-content/uploads/2021/05/linkedin-logo-white-1024x1024.png'></img>
+              </a>
+              <a href='https://github.com/TheoNaulet' target="_blank">
+                <img src={github} ></img>
+              </a>
+              </div>   
+            </div> 
     </div>
   );
 }
