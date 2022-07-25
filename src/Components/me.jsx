917942c90a0749c7f6872d7f68c067b709f360a8
Théo Naulet <theo.naulet@gmail.com>
2022-07-25 12:28:42 +0200

    reload cv

--- src/Components/me.jsx
+++ src/Components/me.jsx
@@ -11,17 +11,19 @@ import "../Styles/me.css"
 const Me = () => {
     return(
         <div className="me">
-        <div className='left-me'>
-            <div className='presentation'>
-            <p className='welcome'>BIENVENUE</p>
-            <h1 className='theo'>Je suis Théo Naulet</h1>
-            <p className='profession'>Étudiant en web-developpement</p>
-            <a id='dl-cv' href='https://drive.google.com/file/d/1Rjv7RNXsObxptL6CArNvKmC5DDvfL_zT/view?usp=sharing' target='blank'>
-                <button className='dl' >Download CV</button>
-            </a>
+            <div className='left-me'>
+                <div className='presentation'>
+                    <p className='welcome'>BIENVENUE</p>
+                    <h1 className='theo'>Je suis Théo Naulet</h1>
+                    <p className='profession'>Étudiant en web-developpement</p>
+                    <a id='dl-cv' href='https://drive.google.com/file/d/1ze9NX6-TtUREAW6e1Gq7d9R99IHZfh0M/view?usp=sharing' target='blank'>
+                        <button className='dl' >Download CV</button>
+                    </a>
+                </div>
             </div>
-        </div>
-            <div className='pic-me'><img src={require("../images/me.png")}></img></div>
+                <div className='pic-me'>
+                    <img src={require("../images/me.png")}></img>
+                </div>
         </div>
     )
 }
